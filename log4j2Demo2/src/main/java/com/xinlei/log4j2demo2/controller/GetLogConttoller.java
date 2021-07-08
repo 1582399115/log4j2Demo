5eb243237a88b4dd900a406145c1c463876f6160
lby <lby@oo.com>
2021-07-08 08:35:22 +0800

    78

--- log4j2Demo2/src/main/java/com/xinlei/log4j2demo2/controller/GetLogConttoller.java
+++ log4j2Demo2/src/main/java/com/xinlei/log4j2demo2/controller/GetLogConttoller.java
@@ -4,6 +4,7 @@ import com.xinlei.log4j2demo2.common.CpsSysLog;
 import com.xinlei.log4j2demo2.service.GetLogServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @Api
+@Slf4j
 public class GetLogConttoller {
     @Autowired
     private GetLogServiceImpl logService;
@@ -19,6 +21,10 @@ public class GetLogConttoller {
     @ApiOperation(value = "这是一个获得生成日志的方法")
     public List<CpsSysLog> getLog(){
         List<CpsSysLog> cpsSysLog=logService.getAllLog();
+        log.info("这是一个获得生成日志的方法");
+        log.debug("这是一个获得生成日志的方法debug测试");
+        log.warn("这是一个获得生成日志的方法warn测试");
+        log.error("这是一个获得生成日志的方法error测试");
         return cpsSysLog;
     }
 }
